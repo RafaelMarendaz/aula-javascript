d00a54c1e0bec91a1455eacb20ed82b23801d19a
RafaelMarendaz <rafael.andrademarendaz@gmail.com>
2021-11-23 18:54:50 -0300

    funções inicias (replace, console, alert)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 var nome = "Rafael A. Marendaz";
 var idade = 26;
+var idade2 = 40;
+var frase = "Japão é o melhor time do mundo";
 alert(nome + " tem " + idade + " anos");
+alert(idade * idade2);
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toLowerCase());
+alert(frase.replace("Japão", "Brasil"))
